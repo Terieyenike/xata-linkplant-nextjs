ed700159ec720b23c100cf3c943850d7f0710cfb
Teri Eyenike <teyenike1@gmail.com>
2023-09-30 02:19:41 +0100

    Update: clean the unused code

--- src/app/page.js
+++ src/app/page.js
@@ -21,10 +21,6 @@ export default function Home() {
           </th>
           <th
             className="px-4 py-3 tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
-            View profile
-          </th>
-          <th
-            className="px-4 py-3 tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
             Edit
           </th>
           <th
@@ -53,23 +49,7 @@ export default function Home() {
               </svg>
             </a>
           </td>
-          <td className="px-4 py-3">
-            <a href="#" target="_blank">
-              name
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4 inline"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-              </svg>
-            </a>
-          </td>
+
           <td className="px-4 py-3 underline text-blue-600"><a href="#">Edit</a></td>
           <td className="px-4 py-3 underline text-red-600"><a href="#">Delete</a></td>
                 </tr>
