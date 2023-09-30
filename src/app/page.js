7f57fd06df65422a95988c10a45575f7e40dedc6
Teri Eyenike <teyenike1@gmail.com>
2023-09-30 01:51:32 +0100

    Feat: get link data from xata

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+// "use client"
 import Link from "next/link"
+import { getXataClient } from "@/xata"
 
+const xata = getXataClient()
 
+export default async function Home() {
+  const details = await xata.db.Link.getAll()
+  const profile = await xata.db.Profile.getAll()
 
-export default function Home() {
   return (
     <main>
     <h1 className="text-4xl text-center my-10">All My Links</h1>
@@ -19,10 +24,7 @@ export default function Home() {
             className="px-4 py-3 tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
             URL
           </th>
-          <th
-            className="px-4 py-3 tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
-            View profile
-          </th>
+
           <th
             className="px-4 py-3 tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
             Edit
@@ -34,11 +36,12 @@ export default function Home() {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-          <td className="px-4 py-3">text</td>
+              {details.map((detail) => (
+                <tr key={detail.id}>
+          <td className="px-4 py-3">{detail.text}</td>
           <td className="px-4 py-3">
-            <a href="#" target="_blank">
-              https://google.com
+            <a href={detail.url} target="_blank">
+              {detail.url}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4 inline"
@@ -53,26 +56,11 @@ export default function Home() {
               </svg>
             </a>
           </td>
-          <td className="px-4 py-3">
-            <a href="#" target="_blank">
-              name
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4 inline"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-              </svg>
-            </a>
-          </td>
+
           <td className="px-4 py-3 underline text-blue-600"><a href="#">Edit</a></td>
           <td className="px-4 py-3 underline text-red-600"><a href="#">Delete</a></td>
                 </tr>
+              ))}
 
               </tbody>
             </table>
